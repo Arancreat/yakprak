e3c84d21bdeffc7f7e0f7e2eb2c4c421086bcf12
Arancreat <arancreat@gmail.com>
2023-01-17 02:56:46 +0900

    Implemented JWT auth

---
+++ backend/src/main/java/ru/yakprak/backend/services/AuthService.java
@@ -0,0 +1,55 @@
+package ru.yakprak.backend.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.yakprak.backend.models.Role;
+import ru.yakprak.backend.models.User;
+import ru.yakprak.backend.repo.UserRepo;
+import ru.yakprak.backend.requests.signInRequest;
+import ru.yakprak.backend.requests.signUpRequest;
+import ru.yakprak.backend.responses.AuthResponse;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse signUp(signUpRequest request) {
+        var user = User.builder()
+                .username(request.getUsername())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .patronymic(request.getPatronymic())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepo.save(user);
+        var jwt = jwtService.generateJwt(user);
+        return AuthResponse.builder()
+                .token(jwt)
+                .build();
+    }
+
+    public AuthResponse signIn(signInRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepo.findByUsername(request.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var jwt = jwtService.generateJwt(user);
+        return AuthResponse.builder()
+                .token(jwt)
+                .build();
+    }
+}
