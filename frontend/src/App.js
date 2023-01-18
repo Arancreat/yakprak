67d10915a073b2f4bc9128e0dda5f1de9c76d5e7
Arancreat <arancreat@gmail.com>
2023-01-19 00:54:00 +0900

    Implemented Sign In and Sign Up pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <header><Navbar /></header>
+      <header>
+        <Navbar />
+      </header>
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/sign_in" element={<SignIn />} />
+          <Route path="/sign_up" element={<SignUp />} />
         </Routes>
       </div>
     </>
---
+++ frontend/src/pages/SignIn.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const SignIn = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
+
+  return (
+    <>
+      <form className="auth_form" onSubmit={handleSubmit}>
+        <h2>Sign in</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign In</button>
+      </form>
+      <div className="sign_up_link">
+        Don't have an account? <Link to="/sign_up">Sign up!</Link>
+      </div>
+    </>
+  );
+};
