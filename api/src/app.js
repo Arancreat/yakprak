5c7ff37536a8ee42373a44f557aa2adc05161b23
Arancreat <arancreat@gmail.com>
2023-04-10 02:21:16 +0900

    Logging when someone uses API

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,14 @@
-import "./cfg.js"
+import "./cfg.js";
+import logger from "./utils/logger.js";
+import chalk from 'chalk';
 import express from "express";
 import userRoute from "./lib/user/route.js";
 import authRoute from "./lib/auth/route.js";
-import logger from "./utils/logger.js";
+
+const loggerMiddleware = (req, res, next) => {
+    logger.info(`${chalk.red(req.method)} "${req.originalUrl}" from remote address: "${req.ip}"`);
+    next();
+};
 
 // parameters
 const PORT = 8080;
@@ -12,13 +18,16 @@ const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(loggerMiddleware);
 
 // routes
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
 try {
-    app.listen(PORT, () => logger.info(`Server ready at http://localhost:${PORT}`));
+    app.listen(PORT, () =>
+        logger.info(`Server ready at http://localhost:${PORT}`)
+    );
 } catch (error) {
     logger.fatal(error);
 }
